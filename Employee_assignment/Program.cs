08ffb560bd3cc9c13dffab64e3e32b09c429623d
venkey <venkeyvenkatesh885@gmail.com>
2020-09-27 17:51:12 +0530

    first commit

---
+++ Employee_assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
