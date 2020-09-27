0632c5e77f3fc389105d76fb9e54783b877d2614
venkey <venkeyvenkatesh885@gmail.com>
2020-09-27 17:53:02 +0530

    [venkey]Refactor. changed the message

--- Employee_assignment/Program.cs
+++ Employee_assignment/Program.cs
@@ -6,7 +6,7 @@ namespace Employee_assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Employee! Welcome");
         }
     }
 }
